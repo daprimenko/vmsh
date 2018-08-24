1b004ba99d70db99375763a68a7b672378e7c791
daprimenko <daprimenko@yandex.ru>
2018-08-24 21:53:35 +0300

    Первый набросок сайта на HTML+CSS. Отдельная страница для каждой новости.

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.main, name='main'),
+    url(r'^news$', views.news_page, name='news_page'),
+    url(r'^news/post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^timetable$', views.timetable, name='timetable'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 
-def post_list(request):
+def main(request):
+    return redirect('news_page')
+
+
+def news_page(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'news/post_list.html', {'posts':posts})
+    return render(request, 'news/news.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'news/post_detail.html', {'post': post})
+
+
+def about(request):
+    return render(request, 'news/about.html', {})
+
+
+def timetable(request):
+    return render(request, 'news/timetable.html', {})
